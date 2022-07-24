e5b18096dfb142ae3fbd59a1602d51eb7d17909f
Pavel <pavel.818@yandex.ru>
2022-07-24 15:17:09 +0400

    BlurView

---
+++ Apple-Music-iOS5-HW/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  Apple-Music-iOS5-HW
+//
+//  Created by Pavel Абрамов on 24.07.2022.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
